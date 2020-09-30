edfc2f5ad441cb0fa6e0998d77c95ccc93ee41f4
BrandNewLifeJackie26 <jackie020130923@126.com>
2020-09-30 12:55:31 +0800

    Finish configuring and testing product framework

--- guolimall-product/src/test/java/com/guoli/guolimall/product/GuolimallProductApplicationTests.java
+++ guolimall-product/src/test/java/com/guoli/guolimall/product/GuolimallProductApplicationTests.java
@@ -1,13 +1,27 @@
 package com.guoli.guolimall.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.guoli.guolimall.product.entity.BrandEntity;
+import com.guoli.guolimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GuolimallProductApplicationTests {
 
+	@Autowired
+	BrandService brandService;
+
 	@Test
 	void contextLoads() {
+		List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+		list.forEach((item) -> {
+			System.out.println(item);
+		});
 	}
 
 }
