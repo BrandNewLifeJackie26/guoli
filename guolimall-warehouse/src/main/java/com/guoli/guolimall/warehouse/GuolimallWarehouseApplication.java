8e518b5669fdd5355b5376861fcebe1603520c92
BrandNewLifeJackie26 <jackie020130923@126.com>
2020-09-30 13:59:32 +0800

    Finish configuring and testing warehouse framework

--- guolimall-warehouse/src/main/java/com/guoli/guolimall/warehouse/GuolimallWarehouseApplication.java
+++ guolimall-warehouse/src/main/java/com/guoli/guolimall/warehouse/GuolimallWarehouseApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GuolimallWarehouseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GuolimallWarehouseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GuolimallWarehouseApplication.class, args);
+    }
 
 }
