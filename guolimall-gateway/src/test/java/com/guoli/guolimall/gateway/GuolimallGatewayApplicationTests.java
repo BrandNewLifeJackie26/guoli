e07a36466c419e2ea6dff5bf0adc6b062b77d70f
BrandNewLifeJackie26 <jackie020130923@126.com>
2020-10-05 19:54:48 +0800

    Set up gateway for all services

---
+++ guolimall-gateway/src/test/java/com/guoli/guolimall/gateway/GuolimallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guoli.guolimall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuolimallGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
